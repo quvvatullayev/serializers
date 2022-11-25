cd4943637a1b94ae741a37414462b0b5027bf12b
quvvatullayev <quvvatullayev@gmail.com>
2022-11-25 11:37:02 +0500

    create update task

--- cor/urls.py
+++ cor/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from cor_app.views import create_task
+from cor_app.views import create_task,update_task
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('addTask/', create_task),
+    path('updateTask/<int:id>', update_task)
 ]
--- cor_app/views.py
+++ cor_app/views.py
@@ -15,3 +15,13 @@ def create_task(request: Request) -> Response:
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['POST'])
+def update_task(request:Request, id) -> Response:
+    data = request.data
+    task = Task.objects.get(id = id)
+    serializer = TaskSerializer(task, data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
