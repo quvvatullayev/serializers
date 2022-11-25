2416ea1407bf4f507e948b853fccf84ee7d14c3a
quvvatullayev <quvvatullayev@gmail.com>
2022-11-25 09:47:52 +0500

    crate models fix

--- cor_app/models.py
+++ cor_app/models.py
@@ -1,34 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.db import models
 
-class Task(models.Model):
-
-    task = models.CharField(max_length=255)
-    description = models.CharField(max_length=50)
-    status = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def to_json(self) -> dict:
-        '''to convert a task object to a dict.
-        
-        Args:
-            self (object): task object.
-        Returns:
-            dict: task as dict.
-        '''
-
-        return {
-            'id': self.id,
-            'title': self.task,
-            'description': self.description,
-            'status': self.status,
-            'created_at': self.created_at,
-            'updated_at': self.updated_at,
-            'user': User.objects.get(username=self.user).username
-        }
-
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=12)
 
     def __str__(self):
-        return self.task
+        return self.username
+
+class Task(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    status = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updateAt = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='task')
+
+    def __str__(self) -> str:
+        return self.name
